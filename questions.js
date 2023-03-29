1633b660d3927f69276ae61ac72c6e358ef3cb0c
Ashutosh2505 <92290543+Ashutosh2505@users.noreply.github.com>
2023-03-29 19:27:27 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,82 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which city is known as Electronic City of India?",
+    answer: "Bangalore",
+    options: [
+      "Mumbai",
+      "Hyderabad",
+      "Guragon",
+      "Bangalore"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The Indian Institute of science is located at",
+    answer: "Bangalore",
+    options: [
+      "Kerala",
+      "Madras",
+      "Bangalore",
+      "New Delhi"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Golden Temple is situated in",
+    answer: "Amritsar",
+    options: [
+      "New Delhi",
+      "Agara",
+      "Amritsar",
+      "Mumbai"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Vijay Stambah is situated in",
+    answer: "Chittor",
+    options: [
+      "Jaipur",
+      "Agra",
+      "Delhi",
+      "Chittor"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The Char Minar is in",
+    answer: "Hyderabad",
+    options: [
+      "Mumbai",
+      "New Delhi",
+      "Pune",
+      "Hyderabad"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+     {
+     numb: 6,
+     question: "What is the capital of India",
+     answer: "New Delhi",
+     options: [
+       "Mumbai",
+       "Kolkata",
+       "Delhi",
+       "New Delhi"
+     ]
+  },
+     {
+     numb: 7,
+     question: "The ratio of width of our National flagto its length is ",
+     answer: "2:3",
+     options: [
+        "3:5"
+		"2:3"
+		"2:4"
+		"3:4"
+     ]
+  },
+];
